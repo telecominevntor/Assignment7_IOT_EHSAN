a13829d3207ede7e6e0fc8c5307ab3d8a7ec4223
telecominevntor <56504728+telecominevntor@users.noreply.github.com>
2020-01-17 22:33:09 +0500

    Add files via upload
    lib.rs of Assignment7

---
+++ lib.rs
@@ -0,0 +1,11 @@
+pub mod judges{
+        pub mod justice{
+        pub fn law(lawyer:String,pending_cases1:i32){
+
+            println!(" ");
+            println!(" (lib.rs) {} : {}",lawyer,pending_cases1);
+            println!(" ");
+
+        }
+    }    
+}
